6d93af14ac551f992a38c9f22dd35f698e7cc5de
Gokul K R <gokulkr652003@gmail.com>
2026-08-02 02:43:59 +0530

    LMA-23 feat(api): add typeorm employee repository

---
+++ apps/backend/src/employees/repositories/employees.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Employee } from '../entities/employee.entity';
+import type { IEmployeesRepository } from '../interfaces/employees-repository.interface';
+
+@Injectable()
+export class TypeOrmEmployeesRepository implements IEmployeesRepository {
+  constructor(
+    @InjectRepository(Employee)
+    private readonly repository: Repository<Employee>,
+  ) {}
+
+  findByEmail(email: string): Promise<Employee | null> {
+    return this.repository.findOne({ where: { email } });
+  }
+
+  create(data: Partial<Employee>): Employee {
+    return this.repository.create(data);
+  }
+
+  save(employee: Employee): Promise<Employee> {
+    return this.repository.save(employee);
+  }
+}
